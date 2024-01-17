96106e1d31645b3c8caecab3a10350e41b3abc1c
Velimir <87824619+VtheSaint@users.noreply.github.com>
2024-01-17 15:42:26 +0300

    base

---
+++ pionsfu.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+// HTTPSDPServer starts a HTTP Server that consumes SDPs
+func HTTPSDPServer(port int) chan string {
+	sdpChan := make(chan string)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := io.ReadAll(r.Body)
+		fmt.Fprintf(w, "done")
+		sdpChan <- string(body)
+	})
+
+	go func() {
+		// nolint: gosec
+		err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	return sdpChan
+}
+
+func main() {
+	chann := HTTPSDPServer(8080)
+	println(chann)
+
+}
