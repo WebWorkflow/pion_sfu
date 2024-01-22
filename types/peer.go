c7cfb5c7a26da2c6196fd7716bc3ddc6f70a64be
Velimir <87824619+VtheSaint@users.noreply.github.com>
2024-01-22 13:07:41 +0300

    Peer interface implemented

--- types/peer.go
+++ types/peer.go
@@ -7,10 +7,45 @@ import (
 	"github.com/pion/webrtc/v3"
 )
 
+type PeerInterface interface {
+	SetSocket(ws_conn *websocket.Conn)
+	AddRemoteTrack(track *webrtc.TrackRemote)
+	RemoveRemoteTrack(track *webrtc.TrackRemote)
+	SetPeerConnection(conn *webrtc.PeerConnection)
+}
+
 type Peer struct {
 	id         string
 	connection *webrtc.PeerConnection
 	streams    map[string]*webrtc.TrackRemote
 	mutex      sync.RWMutex
 	socket     *websocket.Conn
+}
+
+func newPeer(id string) *Peer {
+	return &Peer{id: id, mutex: sync.RWMutex{}}
+}
+
+func (peer *Peer) SetPeerConnection(conn *webrtc.PeerConnection) {
+	peer.mutex.Lock()
+	defer peer.mutex.Unlock()
+	peer.connection = conn
+}
+
+func (peer *Peer) AddRemoteTrack(track *webrtc.TrackRemote) {
+	peer.mutex.Lock()
+	defer peer.mutex.Unlock()
+	peer.streams[track.ID()] = track
+}
+
+func (peer *Peer) RemoveRemoteTrack(track *webrtc.TrackRemote) {
+	peer.mutex.Lock()
+	defer peer.mutex.Unlock()
+	delete(peer.streams, track.ID())
+}
+
+func (peer *Peer) SetSocket(socket *websocket.Conn) {
+	peer.mutex.Lock()
+	defer peer.mutex.Unlock()
+	peer.socket = socket
 }
